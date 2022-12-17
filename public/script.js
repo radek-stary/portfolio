7e61656313861b1961834e4599450420e91cdb10
unknown <hordol122@gmail.com>
2022-12-17 10:15:20 +0100

    repaired responsible navigation

--- public/script.js
+++ public/script.js
@@ -1,19 +1,18 @@
-let timeout;
-
-function copyTelephoneToClipboard() {
-  const telephoneNumber = "+420 731 519 819";
-
-  // Copy the text inside the text field
-  navigator.clipboard.writeText(telephoneNumber);
-
-  $(".alert").addClass("show");
-
-  clearTimeout(timeout);
-
-  timeout = setTimeout(() => {
-    $(".alert").removeClass("show");
-  }, 2000);
+function changePropertiesOnWidthChange() {
+  if (window.matchMedia("(max-width: 990px)").matches) {
+    $("nav").addClass("darkBlueBg");
+    scrollingEffectsEnabled = false;
+    //in this case i should pull the nav
+    $("nav").removeClass("transparent");
+  } else {
+    scrollingEffectsEnabled = true;
+    $("nav").removeClass("darkBlueBg");
+  }
 }
+
+let scrollingEffectsEnabled = false;
+
+let timeout;
 
 function closeAlert() {
   clearTimeout(timeout);
@@ -35,20 +34,27 @@ function copyEmailToClipboard() {
   }, 2000);
 }
 
-var lastScrollTop = 0;
+function copyTelephoneToClipboard() {
+  const telephoneNumber = "+420 731 519 819";
 
-function changeBgOnWidthChange() {
-  if (window.matchMedia("(max-width: 990px)").matches) {
-    $("nav").addClass("darkBlueBg");
-  } else {
-    $("nav").removeClass("darkBlueBg");
-  }
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(telephoneNumber);
+
+  $(".alert").addClass("show");
+
+  clearTimeout(timeout);
+
+  timeout = setTimeout(() => {
+    $(".alert").removeClass("show");
+  }, 2000);
 }
+
+var lastScrollTop = 0;
 
 const windowIsResizedToSmallSize = 0;
 
 $(window).resize(function () {
-  changeBgOnWidthChange();
+  changePropertiesOnWidthChange();
 });
 
 //when website is scrolled
@@ -58,25 +64,29 @@ $(window).scroll(function () {
 
   let scrollingDown = scrollTop > lastScrollTop;
 
-  if (scrollTop < 60) {
-    $("nav").removeClass("blueBg");
-    $("nav").removeClass("transparent");
-  } else if (scrollingDown) {
-    $("nav").addClass("transparent");
-  } else if (!scrollingDown) {
-    $("nav").addClass("blueBg");
-    $("nav").removeClass("transparent");
+  if (scrollingEffectsEnabled) {
+    if (scrollTop < 60) {
+      $("nav").removeClass("blueBg");
+      $("nav").removeClass("transparent");
+    } else if (scrollingDown) {
+      $("nav").addClass("transparent");
+    } else if (!scrollingDown) {
+      $("nav").addClass("blueBg");
+      $("nav").removeClass("transparent");
+    }
   }
 
   lastScrollTop = scrollTop;
 });
 
 $(document).ready(function () {
+  changePropertiesOnWidthChange();
+
   //scroll into contact section
   $(".scrollToContactButton").click(() => {
     $([document.documentElement, document.body]).animate(
       {
-        scrollTop: $(".contact").offset().top,
+        scrollTop: $(".contact").offset().top - $("nav").height(),
       },
       700
     );
@@ -86,7 +96,7 @@ $(document).ready(function () {
   $(".scrollToProjectButton").click(function () {
     $([document.documentElement, document.body]).animate(
       {
-        scrollTop: $(".projects").offset().top,
+        scrollTop: $(".projects").offset().top - $(".navbar").height(),
       },
       700
     );
@@ -96,7 +106,7 @@ $(document).ready(function () {
   $(".scrollToAboutMeButton").click(function () {
     $([document.documentElement, document.body]).animate(
       {
-        scrollTop: $(".aboutMe").offset().top,
+        scrollTop: $(".aboutMe").offset().top - $("nav").height(),
       },
       800
     );
